2cfcc2c25d08d55d2285edfa54a63cbd5fc90bfe
Töpfer, Johann <johanntoepfer@gmail.com>
2023-06-28 22:18:15 +0200

    navigation added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,14 @@
 import { NgModule } from "@angular/core";
 import { RouterModule, Routes } from "@angular/router";
 import { MapComponent } from "./map/map.component";
+import {IndexComponent} from "./index/index.component";
 
 const routes: Routes = [
   {
     path: 'map', component: MapComponent
+  },
+  {
+    path: '', component: IndexComponent
   },
 ];
 
